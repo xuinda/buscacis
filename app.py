2ba173bc17908fb844fcfd9764c9e414ca222fb7
xuinda <154016566+xuinda@users.noreply.github.com>
2024-11-16 10:28:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+import os
 import pandas as pd
+from flask import Flask, request, jsonify
 
+# Inicializando o Flask
 app = Flask(__name__)
 
 # Rota básica para verificar se o servidor está funcionando
@@ -14,33 +16,24 @@ def buscar_ci():
     # Pega o termo de busca do parâmetro da URL
     termo = request.args.get('termo', '')
     
-    # Caminho completo do arquivo Excel
-    caminho_arquivo = r'D:\RICARDO\esquemas txt\app\saida.xlsx'  # Substitua pelo seu caminho exato
+    # Caminho do arquivo Excel, utilizando variável de ambiente
+    caminho_arquivo = os.getenv('CAMINHO_ARQUIVO', 'saida.xlsx')  # 'saida.xlsx' é o valor padrão
     
     try:
         # Carregando o arquivo Excel (.xlsx) com pandas
-        df = pd.read_excel(caminho_arquivo)  # Carrega o arquivo Excel
-        
-        # Verificando as colunas disponíveis
-        print("Colunas encontradas:", df.columns.tolist())
-        
-        # Verificando se a coluna 'conteudo' existe
-        if 'conteudo' not in df.columns:
-            return jsonify({"error": "A coluna 'conteudo' não foi encontrada no arquivo Excel."}), 400
+        df = pd.read_excel(caminho_arquivo)
         
         # Filtrando os resultados que contêm o termo de busca na coluna 'conteudo'
         resultados = df[df['conteudo'].str.contains(termo, case=False, na=False)]
         
-        # Se não houver resultados, retornar uma mensagem apropriada
-        if resultados.empty:
-            return jsonify({"message": "Nenhum resultado encontrado."}), 404
-        
         # Retorna os resultados como uma resposta JSON
         return jsonify(resultados[['nome_arquivo', 'conteudo']].to_dict(orient='records'))
-    
     except Exception as e:
-        # Caso ocorra algum erro, retorna a mensagem de erro
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Obtém a porta do ambiente ou usa 5000 como padrão
+    port = int(os.environ.get('PORT', 5000))
+    
+    # Configura o Flask para escutar na porta correta
+    app.run(host='0.0.0.0', port=port)
